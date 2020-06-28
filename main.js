b5fffadfa2dfa9eb1b34d7dc6fde8a7051e1576e
naetoncsgo <65255548+naetoncsgo@users.noreply.github.com>
2020-06-28 02:35:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	var fel = true;
+	
 	$('#header').slideUp();
 	$('#main-welcome').fadeIn('slow');
 	$('#drop-button').click(function(){
 		$('#header').slideToggle();
-		$(this).attr('value', '^');
 		if(fel == true){
 			$('#drop-button').text('▲');
 			fel = false;
