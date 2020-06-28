5a706e2de2b3f219dd4baf0ef8ad0d258dccd898
naetoncsgo <65255548+naetoncsgo@users.noreply.github.com>
2020-06-28 02:32:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -18,6 +18,13 @@ $(document).ready(function(){
         $(this).bind("contextmenu", function(e) {
             e.preventDefault();
         });
-    }); 
+    });
+	$(document).keydown(function (event) {
+    if (event.keyCode == 123) {
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {     
+        return false;
+    }
+});
 
 });
